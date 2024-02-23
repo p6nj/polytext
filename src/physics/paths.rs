b5af89bb207f7a25a505bea06e951e8334ddb346
p6nj <breee@duck.com>
2024-02-23 16:07:02 +0100

    turn it into an iterator impl

--- src/physics/paths.rs
+++ src/physics/paths.rs
@@ -1,3 +1,5 @@
+use derive_new::new;
+
 use super::{Element, Event};
 
 struct Pixel {
@@ -5,9 +7,8 @@ struct Pixel {
     path: Vec<Event>,
 }
 
+#[derive(new)]
 struct PixelPath {
-    /// The asked states this path goes though
-    states: Vec<bool>,
     from: Element,
     path: Vec<Vec<Event>>,
     /// Other paths this one is compatible with.
@@ -16,8 +17,18 @@ struct PixelPath {
     works_with: Vec<PixelPath>,
 }
 
-impl PixelPath {
-    fn filter_compatible<'a>(&self, paths: &'a Vec<PixelPath>) -> Vec<&'a PixelPath> {
-        paths.iter().filter(|p| todo!()).collect()
+trait PixelPathFilter<'a>: Iterator<Item = &'a PixelPath> + Sized {
+    fn filter_compatible(self, with: PixelPath) -> impl Iterator<Item = &'a PixelPath> {
+        self.filter(|p| todo!())
     }
+}
+
+impl<'a, T> PixelPathFilter<'a> for T where T: Iterator<Item = &'a PixelPath> + Sized {}
+
+fn test() {
+    let a: Vec<&PixelPath> = PixelPath::new(Element::Block, vec![], vec![])
+        .works_with
+        .iter()
+        .filter_compatible(PixelPath::new(Element::Block, vec![], vec![]))
+        .collect();
 }
